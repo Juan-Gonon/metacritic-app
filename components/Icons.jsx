5c657df997205a0fb132f240a336cb34842ec4d4
Juan Gonon <juanfegonon27@gmail.com>
2024-07-27 23:22:05 -0600

    error icon

--- components/Icons.jsx
+++ components/Icons.jsx
@@ -1,10 +1,10 @@
 import { FontAwesome5 } from "@expo/vector-icons";
 import { FontAwesome } from "@expo/vector-icons";
 
-export const CircleInfoIcon = (props) => (
+export const HomeIcon = (props) => (
   <FontAwesome name="home" size={24} color="white" {...props} />
 );
 
-export const HomeIcon = (props) => (
+export const CircleInfoIcon = (props) => (
   <FontAwesome5 name="info-circle" size={24} color="white" {...props} />
 );
